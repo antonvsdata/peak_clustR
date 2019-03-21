616d99026706d587144a770dddb7bf038d67319d
Anton Mattsson <antom@uef.fi>
2019-03-21 14:09:26 +0200

    Updated testing script

--- test_data.R
+++ test_data.R
@@ -1,5 +1,6 @@
 library(openxlsx)
 library(ggplot2)
+library(ggrepel)
 
 
 setwd("//research/antom/Projects/PeakCluster/peak_clustR")
@@ -10,22 +11,22 @@ X <- read.xlsx("HILIC_NEG_TEST.xlsx", sheet = 1)
 P <- read.xlsx("HILIC_NEG_TEST.xlsx", sheet = 2)
 
 
-conn <- find_connections(data = X, peaks = P, corr_thresh = 0.85, rt_window = 1/60, name_col = "Name", mz_col = "mz", rt_col = "rt")
+conn <- find_connections(data = X, features = P, corr_thresh = 0.85, rt_window = 1/60, name_col = "Name", mz_col = "mz", rt_col = "rt")
 
 clusters <- find_clusters(conn)
 
-pulled <- pull_peaks(clusters, data= X, peaks = P, name_col = "Name")
+pulled <- pull_features(clusters, data= X, features = P, name_col = "Name")
 cdata <- pulled$cdata
-cpeaks <- pulled$cpeaks
+cpeaks <- pulled$cfeatures
 
 multisheet_xlsx(dfs = pulled, filename = "HILIC_NEG_output2.xlsx")
 
 
 cluster <- clusters[[2]]
-length(cluster$peaks)
+length(cluster$features)
 
 P$MPA <- sapply(X[P$Name], median, na.rm = TRUE)
-plot_cluster(peaks = P, cluster, name_col = "Name", mz_col = "mz", rt_col = "rt")
+plot_cluster(features = P, cluster, name_col = "Name", mz_col = "mz", rt_col = "rt")
 
 
 net <- igraph_to_networkD3(cluster$graph)
@@ -33,5 +34,9 @@ saveNetwork(forceNetwork(Links = net$links, Nodes = net$nodes, NodeID = "name", 
             "net.html")
 
 source("visualizations.R")
-plot_intensities(P, cluster, name_col = "Name", mz_col = "mz", rt_col = "rt")
+plot_features(P, cluster, name_col = "Name", mz_col = "mz", rt_col = "rt", rt_window = 1/60)
 
+plot_heatmaps(data = X, features = P, name_col = "Name", mz_col = "mz", rt_col = "rt")
+
+visualize_clusters(data = X, features = P, clusters = clusters, rt_window = 1/60, name_col = "Name", mz_col = "mz", rt_col = "rt",
+                   file_path = "figures/")
